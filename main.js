36a95d91081cba7fa2f4b8400f6927bf33a7c1df
zgh <ghahremans908@gmil.com>
2025-12-04 18:10:29 +0330

    add the if and else

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const number = Number(prompt("enter").value);
-const x = 8;
+const number = Number(prompt("یک عدد بین 1 تا 10 حدس بزن ")).value;
+function guessing() {
+  const x = 8;
 
-if (number >= 0 || number < 10) {
+  if (number <= 0 || number >= 10) {
+    message.textContent = "عدد باید بین 0تا 10 باشد ";
+    message.style.color = "red";
+    return;
+  } else {
+    if ((number = x)) {
+      message.textContent = "درست حدس زدید ";
+      message.style.color = "gold";
+      return;
+    } else {
+      message.textContent = "اشتباه حدس زدید دوباره امتحان کنید ";
+      return;
+    }
+  }
 }
