612d5e73a0d3ed1fc307d269a4137e013b10b735
zgh <ghahremans908@gmil.com>
2025-12-04 18:23:00 +0330

    delete value

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const number = Number(prompt("یک عدد بین 1 تا 10 حدس بزن ")).value;
+const number = Number(prompt("یک عدد بین 1 تا 10 حدس بزن "));
 function guessing() {
   const x = 8;
 
   if (number <= 0 || number >= 10) {
-    message.textContent = "عدد باید بین 0تا 10 باشد ";
-    message.style.color = "red";
+    alert("عدد باید بین 0تا 10 باشد ");
+    // message.style.color = "red";
     return;
   } else {
     if ((number = x)) {
-      message.textContent = "درست حدس زدید ";
-      message.style.color = "gold";
+      alert("درست حدس زدید ");
+      // message.style.color = "gold";
       return;
     } else {
-      message.textContent = "اشتباه حدس زدید دوباره امتحان کنید ";
+      alert("اشتباه حدس زدید دوباره امتحان کنید ");
       return;
     }
   }
