d4955f2009e34127681c0fd78df21f2e735264cb
zgh <ghahremans908@gmil.com>
2025-12-06 17:02:27 +0330

    add number of user attempts

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const x = 2;
+let attempts = 0;
 let i;
 
 while (i != x) {
-  let i = Number(prompt("یک عدد بین 0 تا 10 حدس بزن "));
+  i = Number(prompt("یک عدد بین 0 تا 10 حدس بزن "));
 
   if (i <= 0 || i >= 10) {
     alert("عدد باید بین 0 تا 10 باشد ");
   } else {
+    attempts++;
     if (i == x) {
-      alert("درست حدس زدید ");
+      alert(`بعد از${attempts} تلاش درست حدس زدید `);
+
       break;
     } else if (i > x) {
       alert("حدس شما بزرگ تر از عدد مورد نظر  دوباره امتحان کنید ");
